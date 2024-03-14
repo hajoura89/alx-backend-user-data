aee79dc972ea8dbf918032c99a9203f7ef24f558
hajoura89 <hajarkammou@gmail.com>
2024-03-14 23:00:24 +0000

    Encrypting passwords

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,7 +17,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
-    """Filtering a log line"""
+    """Filters a log line"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
 
